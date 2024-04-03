12bf36bb7429652070eee244a1c4817fbb21c2c4
Qendresa-Potoku <94839915+Qendresa-Potoku@users.noreply.github.com>
2024-04-04 01:12:23 +0200

    footer style

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,25 @@
 export function initFooter(div) {
   div.innerHTML = `
-    <div class="container py-5">
-         <div class="row">
-            <div class="col-6">
-            Copyrights &copy; MakeUp, 2024.
-            </div>
-            <div class="col-6">
-                <p class="float-end">Powered by <a href="#">JS Module</a></p>
-            </div>
-            </div>
-        </div>`;
+    <div class="footer">
+<div class="column">
+<a href="#"><i class="bi bi-facebook"></i></a>
+<a href="#"><i class="bi bi-instagram"></i></a>
+<a href="#"><i class="bi bi-twitter-x"></i></a>
+
+</div>
+
+<div class="column">
+<ul>
+<li><a href="#">Contact us</a></li>
+<li><a href="#">Our Services</a></li>
+<li><a href="#">Privacy Policy</a></li>
+<li><a href="#">Terms & Conditions</a></li>
+<li><a href="#">Career</a></li>
+</ul>
+</div>
+
+<div class="row d-flex justify-content-center">
+INFERNO Copyright © 2021 Inferno - All rights reserved || Designed By: Mahesh 
+</div>
+</div>`;
 }
